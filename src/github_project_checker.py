fbb1270a622a9292ae27fba5be3bf86c5c37ca6b
Yigit Ozkan <yigitozkan2804@gmail.com>
2011-10-08 22:34:02 +0200

    final changes done ;)

--- src/github_project_checker.py
+++ src/github_project_checker.py
@@ -12,13 +12,14 @@ Created on Mon Sep 26 22:17:12 2011
 """
 
 import os
+import commands
 
-def get_home_path():
+def get_home_path(): #gets the home path of the user
     home_dir = os.getenv("HOME")
     #print "home path is: "+home_dir
     return home_dir
 
-def get_all_subdirs(directory):
+def get_all_subdirs(directory): #adds full path to dir and then returns list of subdirectories
     list_of_subdirs = os.walk(directory).next()[1]
     subdirs=[]
     for subdir in list_of_subdirs:
@@ -27,19 +28,30 @@ def get_all_subdirs(directory):
     #print subdirs
     return subdirs
     
-def get_all_github_dirs(subdirs):
-    for subdir in subdirs:
-        if ".git" in get_all_subdirs(subdir):
-            print "github repo found: "+subdir
-    
+def get_subdirs(directory): #simply gets dirs :)
+    subdirs_without_path=os.walk(directory).next()[1]
+    return subdirs_without_path
     
 def main():
+    os.system('clear')
     home = get_home_path()
-    print "home path is: "+home
+    #print "home path ready to be analyzed: "+home
     subdirs = get_all_subdirs(home)
-    print "all subdirs have been analyzed."
-    get_all_github_dirs(subdirs)
-    print "all github directories have been analyzed."
-
+    #print "all subdirs ready to be analyzed."
+    github_dirs=[]
+    #print "github repos found:"
+    for subdir in subdirs:
+        if ".git" in get_subdirs(subdir):
+            #print subdir
+            if "project_checker" not in subdir:
+                github_dirs.append(subdir)
+    for project in github_dirs:
+        #print "            "+project
+        output = commands.getoutput('cd '+ project + '; git status')
+        #print output
+        if 'clean' not in output:
+            print 'problem with '+project
+    print "ALL SET, CAPTAIN!"
+    
 if __name__ == '__main__':
     main()
